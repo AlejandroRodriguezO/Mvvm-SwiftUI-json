5bab29a284e195df8a9ca5f4c964aeac412e4c08
jorgealejandrorodriguezoviedo <alejo.rodriguez@experimentality.co>
2022-11-05 14:00:47 -0500

    Initial Commit

---
+++ MCSwiftUI/MCSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MCSwiftUIApp.swift
+//  MCSwiftUI
+//
+//  Created by Jorge Alejandro Rodriguez Oviedo on 5/11/22.
+//
+
+import SwiftUI
+
+@main
+struct MCSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
